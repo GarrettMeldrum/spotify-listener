7d2f810d23521e6b6f470460a23c420ee729f8fe
Garrett Meldrum <garrettmeldrum@icloud.com>
2026-08-11 11:41:01 -0600

    there was an error in the reauth.py script

--- scripts/ReAuth.py
+++ scripts/ReAuth.py
@@ -11,6 +11,11 @@ REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 # need to match with app.py
 CACHE_PATH = '/app/data/.cache'
 
+# Remove the old (dead) token so get_access_token doesn't try to
+# validate/refresh it before using the fresh authorization code.
+if os.path.exists(CACHE_PATH):
+    os.remove(CACHE_PATH)
+
 auth_manager = SpotifyOAuth(
     client_id = CLIENT_ID,
     client_secret = CLIENT_SECRET,
@@ -26,6 +31,6 @@ print(auth_url)
 
 response_url = input("\nPaste the full redirect url").strip()
 code = auth_manager.parse_response_code(response_url)
-auth_manager.get_access_token(code, as_dict=False)
+auth_manager.get_access_token(code, as_dict=False, check_cache=False)
 
 print("Reauth successful")
